2b81deb8cfcbc92792d70187f239e916a033b853
Hongkai Liu <hongkailiu@users.noreply.github.com>
2026-05-27 09:57:11 -0400

    Remove a TODO

--- cmd/co-conditions-bugs/main.go
+++ cmd/co-conditions-bugs/main.go
@@ -136,7 +136,6 @@ func parseJiraTickets(content []byte) []string {
 }
 
 // TODO: commit output to GitHub
-// TODO: strikethrough if confirmed
 var notes = map[string]string{
 	"OCPBUGS-22382": "Won't Do confirmed",
 	"OCPBUGS-23744": "Won't Do confirmed: OLMv0 in maintenance mode",
